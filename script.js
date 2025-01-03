d322bf7cb04cb9ca978e2e42b4da853cefa12722
bhawana-web <bhawnav161102@gmail.com>
2025-01-03 22:49:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,46 +1,52 @@
-//complete this code
-// Base Animal class
+// Animal class definition
 class Animal {
   constructor(species) {
-    this._species = species;
+    this._species = species; // Use underscore to indicate private-like variable
   }
 
-  // Getter for species
+  // Getter method for species
   get species() {
     return this._species;
   }
 
-  // Method to log the sound of the animal
+  // Method to log the sound the animal makes
   makeSound() {
-    console.log(The ${this._species} makes a sound);
+    console.log(`The ${this.species} makes a sound`);
   }
 }
 
-// Dog class extending Animal
-class Dog extends Animal {
-  constructor(species) {
-    super(species);
-  }
-
-  // Method to log "woof"
-  bark() {
-    console.log("woof");
-  }
-}
-
-// Cat class extending Animal
+// Cat class that extends Animal
 class Cat extends Animal {
   constructor(species) {
-    super(species);
+    super(species); // Call the parent class constructor
   }
 
-  // Method to log "purr"
+  // Method specific to Cat that logs "purr"
   purr() {
     console.log("purr");
   }
 }
 
-// Do not change the code below this line
-window.Animal = Animal;
-window.Dog = Dog;
-window.Cat = Cat;
+// Dog class that extends Animal
+class Dog extends Animal {
+  constructor(species) {
+    super(species); // Call the parent class constructor
+  }
+
+  // Method specific to Dog that logs "woof"
+  bark() {
+    console.log("woof");
+  }
+}
+
+// Example usage:
+
+// Creating an instance of Cat
+const myCat = new Cat("Siamese");
+myCat.makeSound(); // Output: The Siamese makes a sound
+myCat.purr();      // Output: purr
+
+// Creating an instance of Dog
+const myDog = new Dog("Golden Retriever");
+myDog.makeSound(); // Output: The Golden Retriever makes a sound
+myDog.bark();      // Output: woof
